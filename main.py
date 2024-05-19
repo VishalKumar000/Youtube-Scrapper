16f7ccca65d7ab943eb2450e5d8a906216298d1f
VKumar786 <vkumar@onemindservies.com>
2024-05-19 12:52:48 +0530

    -

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+'''
+yt-dlp --dateafter 20240512 --datebefore $(date +%Y%m%d) --write-thumbnail -o "/home/bakru_k78/Videos/%(title)s - @%(uploader)s/%(title)s.%(ext)s" https://www.youtube.com/@PandaShorts/shorts
+'''
+
+'''
+yt-dlp --write-thumbnail -o "/home/bakru_k78/Videos/%(title)s - @%(uploader)s/%(title)s.%(ext)s" https://www.youtube.com/@PandaShorts/shorts
+'''
+
+# https://www.youtube.com/@DewLoyd_/shorts 20240517
+# https://www.youtube.com/@jeffreyxreacts/shorts 20240517
+# https://www.youtube.com/@YoutubeNetFlex/shorts 20240519
+# https://www.youtube.com/@funnyclipsontheinternet01/shorts 20240519
+# https://www.youtube.com/@SigmaUltra/shorts 20240519
+# https://www.youtube.com/@Piicaro/shorts 20240519
+# https://www.youtube.com/@WtfBroClips000/shorts 20240519
+# https://www.youtube.com/@Ohio.Cat.Bombastic/shorts 20240519
+# https://www.youtube.com/@Sakura-ShinHye/shorts 20240519
+# https://www.youtube.com/@Comment_Reading_Department./shorts 20240519
+# https://www.youtube.com/@amemus_/shorts 20240519
+# https://www.youtube.com/@akselsili/shorts 20240519
+# https://www.youtube.com/@Bebahan/shorts 20240519
+# https://www.youtube.com/@PandaShorts/shorts 20240519
